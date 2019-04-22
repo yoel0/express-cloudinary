bca2dd3ddaca76e2cf6d1c96a872ce44a4f43405
brandiw <brandimichelle.0911@gmail.com>
2019-04-22 11:30:51 -0700

    Adding solution

--- index.js
+++ index.js
@@ -1,12 +1,53 @@
-var express = require('express');
-var ejsLayouts = require('express-ejs-layouts');
-var app = express();
+// Include the variables from .env
+require('dotenv').config()
 
-app.set('view engine', 'ejs');
-app.use(ejsLayouts);
+// Require needed modules
+let cloudinary = require('cloudinary')
+let express = require('express')
+let layouts = require('express-ejs-layouts')
+let multer = require('multer')
 
-app.get('/', function(req, res) {
-  res.render('index');
-});
+// Set up multer's storage/memory locations
+let storage = multer.diskStorage({
+  filename: (req, file, cb) => {
+    cb(null, `${file.fieldname}-${Date.now()}`)
+  },
+  destination: (req, file, cb) => {
+    cb(null, '/tmp/my-uploads')
+  }
+})
+let upload = multer({ storage: storage })
 
-app.listen(3000);
+// Declare express app
+let app = express()
+
+// Settings and middleware
+app.set('view engine', 'ejs')
+app.use(layouts)
+
+// GET / Home page
+app.get('/', (req, res) => {
+  res.render('index', { recentUpload: '' })
+})
+
+// POST / A place to send the image data
+app.post('/', upload.single('myFile'), (req, res) => {
+  cloudinary.uploader.upload(req.file.path, result => {
+    res.render('index', { recentUpload: result.secure_url })
+  })
+})
+
+// Listen on port 3000
+app.listen(3000)
+
+
+
+
+
+
+
+
+
+
+
+
