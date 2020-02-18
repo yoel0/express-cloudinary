e90b362cf817a993b36dccb6e1381ea55d1555de
azocher <anna.zocher@gmail.com>
2020-02-17 21:07:26 -0800

    added file upload capability

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var ejsLayouts = require('express-ejs-layouts');
 var app = express();
+var multer = require('multer');
+var upload = multer({ dest: './uploads/' });
 
 app.set('view engine', 'ejs');
 app.use(ejsLayouts);
@@ -9,4 +11,7 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 
+app.post('/', upload.single('inputFile'), function(req, res) {
+  res.send(req.file);
+});
 app.listen(3000);
