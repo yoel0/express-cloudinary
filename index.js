91df0a1eca7524662a7ba3b6565c3003d17491e0
azocher <anna.zocher@gmail.com>
2020-02-17 21:26:24 -0800

    added upload to cloudinary feature

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
 var express = require('express');
 var ejsLayouts = require('express-ejs-layouts');
 var app = express();
 var multer = require('multer');
 var upload = multer({ dest: './uploads/' });
+var cloudinary = require('cloudinary');
 
 app.set('view engine', 'ejs');
 app.use(ejsLayouts);
@@ -12,6 +14,12 @@ app.get('/', function(req, res) {
 });
 
 app.post('/', upload.single('inputFile'), function(req, res) {
-  res.send(req.file);
+  cloudinary.uploader.upload(req.file.path, function(result) {
+    res.send(result);
+  });
 });
+
+
+
+
 app.listen(3000);
