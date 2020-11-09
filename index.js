9eece1ea2db3afbacb881e72c6b0247ca557aab6
yoel <67129044+yoel0@users.noreply.github.com>
2020-11-09 13:39:08 -0800

    [forAdam] only, async is great

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
-var express = require('express');
-var ejsLayouts = require('express-ejs-layouts');
-var app = express();
+require("dotenv").config();
+const express = require("express");
+const ejsLayouts = require("express-ejs-layouts");
+const app = express();
+const multer = require("multer");
+const upload = multer({ dest: "./uploads" });
+const cloudinary = require("cloudinary");
+const db = require("./models");
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(ejsLayouts);
+app.use(express.static("static"));
+cloudinary.config(process.env.CLOUDINARY_URL);
 
-app.get('/', function(req, res) {
-  res.render('index');
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
+app.post("/", upload.single("myFile"), (req, res) => {
+  cloudinary.uploader.upload(req.file.path, (result) => {
+    db.cloudpic
+      .findOrCreate({
+        where: { url: result.url },
+      })
+      .then(() => {
+        res.redirect("/show");
+      })
+      .catch((err) => {
+        console.log("ERROR:", err);
+      });
+  });
+});
+
+app.get("/show", (req, res) => {
+  db.cloudpic
+    .findAll()
+    .then((myPics) => {
+      res.render("show", { myPics });
+    })
+    .catch((err) => {
+      console.log("ERROR:", err);
+    });
 });
 
 app.listen(3000);
